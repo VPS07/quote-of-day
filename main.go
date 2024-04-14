8e2cfbbee3758e14e68f85bfa99e97dc6de27fee
Vivek <62896712+VPS07@users.noreply.github.com>
2024-04-14 15:59:38 +0000

    Add quote scraping logic

--- main.go
+++ main.go
@@ -2,47 +2,31 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
+	"github.com/gocolly/colly"
 )
 
 func main() {
-	// Define the target URL
+	var quote string
+	
 	url := "https://www.bing.com/search?q=Quote%20of%20the%20day"
 
-	// Create a new HTTP client
-	client := &http.Client{}
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.bing.com"),
+	)
 
-	// Make an HTTP GET request
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+		// extract the quote from the page
+		c.OnHTML(".bt_qotd", func(e *colly.HTMLElement) {
+			res := e.ChildText("div.bt_quoteText")
+			
+			//if quote not found
+			if len(res) < 1 {
+				return
+			}
 
-	// Send the request and get the response
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	defer resp.Body.Close() // Ensure proper closing of the body
-
-	// Check for successful response (status code 200)
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error:", resp.StatusCode)
-		return
-	}
-
-	// Read the response body into a byte slice
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Print the first few bytes of the response to show content retrieval
-	fmt.Println("Fetched content (first 500 bytes):")
-	fmt.Println(string(body[:500])) // Limit output to 500 bytes
+			quote = res
+	
+			fmt.Println("\n"+"“"+quote+"”"+"\n")
+		})
+	
+	c.Visit(url)
 }
